bce4914e6d377a6a2f1a2f4dd4df2cac348405a1
J. Nathan Matias <nathan@daffodil.local>
2009-10-26 14:23:55 +0000

    Completed unilives-videostream plugin

--- unilives-videostream.php
+++ unilives-videostream.php
@@ -32,12 +32,16 @@ class UniLivesVideostreamWidget extends WP_Widget {
     $title = apply_filters('widget_title', $instance['title'] );
 
     echo $before_widget;
+    
+    echo $before_title;
+    echo $title;
+    echo $after_title;
 
     //TODO: make the number of shown posts configurable
     $videoposts = new WP_Query(array(
       'category_name' => "video", 
       'meta_key' => "video-thumb",
-      'show_posts' => 4
+      'posts_per_page' => $instance['show'] 
     ));
     if( $videoposts->have_posts() ) {
       while ($videoposts->have_posts()) : 
@@ -51,7 +55,7 @@ class UniLivesVideostreamWidget extends WP_Widget {
         $university = get_post_meta(get_the_ID(), 'video-university', true);
         $location = get_post_meta(get_the_ID(), 'video-location', true);
 ?>
-<a href="<?php the_permalink();?>"><div class="unilives_video"><img src="<?php echo $url ?>"/><div class="unilives_video_info"><div class="unilives_video_title"><?php the_title();?></div><div class="unilives_video_author"><?php echo $author?></div><div class="unilives_video_duration"><?php echo $duration?></div><div class="unilives_video_university"><?php echo $university?></div><div class="unilives_video_location"><?php echo $location?></div><div class=""><?php the_excerpt();?></div></div></div></a>
+<a href="<?php the_permalink();?>"><div class="unilives_video"><img src="<?php echo $url ?>" height=<?php echo  $instance['height']?>/><div class="unilives_video_info"><div class="unilives_video_title"><?php the_title();?></div><div class="unilives_video_author"><?php echo $author?></div><div class="unilives_video_duration"><?php echo $duration?></div><div class="unilives_video_university"><?php echo $university?></div><div class="unilives_video_location"><?php echo $location?></div><div class=""><?php the_excerpt();?></div></div></div></a>
 <?
       endwhile;
     }
@@ -60,17 +64,31 @@ class UniLivesVideostreamWidget extends WP_Widget {
     return $instance;
   }
  
+  // cribbed from the twitter widget, my this is a disaster waiting to happen, especially the display of forms
   function form( $instance ) {
-    $instance = wp_parse_args( (array) $instance, array('account' => '', 'title' => '', 'show' => '5', 'hidereplies' => false) );
+    $instance = wp_parse_args( (array) $instance, array('title' => '', 'show' => '4', 'height' => '100') );
     $title = esc_attr($instance['title']);
     $show = absint($instance['show']);
     if ( $show < 1 || 20 < $show )
-       $show = '5';
-   $height = absint($instance['height']);
+       $show = '4';
+    $height = absint($instance['height']);
     if ( $height < 1 || 300 < $height )
-       $width = '80';
+      $heigh = '100';
 
- 
+    echo '<p><label for="' . $this->get_field_id('title') . '">' . __('Title:') . '
+          <input class="widefat" id="' . $this->get_field_id('title') . '" name="' . $this->get_field_name('title') . '" type="text" value="' . $title . '" />
+          </label></p>
+          <p><label for="' . $this->get_field_id('show') . '">' . __('Max posts to show:') . '
+                        <select id="' . $this->get_field_id('show') . '" name="' . $this->get_field_name('show') . '">';
+                for ( $i = 1; $i <= 20; ++$i )
+                        echo "<option value='$i' " . ( $show == $i ? "selected='selected'" : '' ) . ">$i</option>";
+
+                echo '          </select>
+          </label></p>
+          <p><label for="' . $this->get_field_id('height') . '">' . __('Common height of thumbnails:') . '
+          <input class="widefat" id="' . $this->get_field_id('height') . '" name="' . $this->get_field_name('height') . '" type="text" value="' . $height . '" />
+          </label></p>';
+         
   }
 }
 ?>
